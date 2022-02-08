5135ee70e9a4c579fc9cb2f61b9a2297bc55774e
braiso-22 <braisfv22@gmail.com>
2022-02-08 17:58:53 +0100

    Enemigo ataca

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float speed = 3.0f;
+    Rigidbody enemyRb;
+    GameObject player;
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+
+
+    void Update()
+    {
+        Vector3 moveDirection = (player.transform.position
+        - transform.position).normalized;
+        enemyRb.AddForce(moveDirection * speed);
+    }
+}
